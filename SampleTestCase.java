22670861bc96a3d32e56370cdb36a9312035d12a
suja2494 <124678391+suja2494@users.noreply.github.com>
2024-02-28 20:13:41 +0530

    Added a new Samplefile

---
+++ SampleTestCase.java
@@ -0,0 +1,23 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class SampleTestCase {
+	
+	@Test
+	public void firstTestCase() {
+		System.out.println("First Test Case");
+	}
+	@Test
+	public void secondTestCase() {
+		System.out.println("Second Test Case");
+	}@Test
+	public void thirdTestCase() {
+		System.out.println("Third Test Case");
+	}
+	@Test
+	public void fourthTestCase() {
+		System.out.println("Fourth Test Case");
+	}
+
+}
